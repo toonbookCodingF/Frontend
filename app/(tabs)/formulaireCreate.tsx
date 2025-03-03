8db8be82bc234c3eaba217a292b9152837367864
Valentin Cheel <valentin.cheel.pro@gmail.com>
2025-03-03 23:06:06 +0100

    boutton upload and save

--- app/(tabs)/formulaireCreate.tsx
+++ app/(tabs)/formulaireCreate.tsx
@@ -7,16 +7,8 @@ export default function MyForm() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Catégorie d'oeuvre :</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Entrez le nom de la catégorie"
-        placeholderTextColor="#aaa"
-        value={inputText}
-        onChangeText={setInputText} // Met à jour la valeur
-      />
 
-
+      <Text style={styles.title}>Formulaire</Text>
 
       <Text style={styles.label}>Nom de l'oeuvre:</Text>
             <TextInput
@@ -24,9 +16,16 @@ export default function MyForm() {
               placeholder="Entrez le nom de l'oeuvre'"
               placeholderTextColor="#aaa"
               value={inputText}
-              onChangeText={setInputText} // Met à jour la valeur
+              onChangeText={setInputText}
             />
-
+      <Text style={styles.label}>Catégorie d'oeuvre :</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Entrez le nom de la catégorie"
+              placeholderTextColor="#aaa"
+              value={inputText}
+              onChangeText={setInputText} // Met à jour la valeur
+             />
 
       <Text style={styles.label}>Description de l'oeuvre :</Text>
             <TextInput
@@ -35,12 +34,15 @@ export default function MyForm() {
               placeholderTextColor="#aaa"
               value={inputText}
               multiline={true}
-              onChangeText={setInputText} // Met à jour la valeur
+              onChangeText={setInputText}
             />
+      <Text style={styles.labelCover}>Cover</Text>
+      <Text style={styles.buttonUpload}>Upload</Text>
+
+      <Text style={styles.buttonSave}>Save</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -49,18 +51,31 @@ const styles = StyleSheet.create({
     backgroundColor: "#800080",
     padding: 20,
   },
+  title: {
+      fontSize: 18,
+      fontWeight: "bold",
+      marginBottom: 15,
+      color: 'white',
+  },
   label: {
     fontSize: 18,
     marginBottom: 8,
+    color: 'white',
   },
+  labelCover: {
+      fontSize: 18,
+      marginBottom: 0,
+      color: 'white',
+    },
   input: {
     width: "80%",
     height: 40,
     borderWidth: 2,
-    borderColor: "#FF69B4", 
+    borderColor: "#FF69B4",
     borderRadius: 10,
     paddingHorizontal: 10,
     backgroundColor: "white",
+    marginBottom: 30,
   },
   result: {
     marginTop: 15,
@@ -69,12 +84,33 @@ const styles = StyleSheet.create({
   },
   descriptionInput: {
     width: "80%",
-    height: 100, // Augmente la hauteur
+    height: 200,
     borderWidth: 2,
     borderColor: "#FF69B4",
     borderRadius: 10,
-    paddingHorizontal: 10,
+    paddingHorizontal: 20,
     backgroundColor: "white",
-    textAlignVertical: "top", // Le texte commence en haut
+    textAlignVertical: "top",
+    marginBottom: 30,
   },
+  buttonUpload: {
+      textAlign: "center",
+      backgroundColor: "#FF69B4",
+      padding: 15,
+      borderRadius: 25,
+      width: "30%",
+      alignItems: "center",
+      marginTop: 10,
+    },
+  buttonSave: {
+        textAlign: "center",
+        backgroundColor: "#FF69B4",
+        padding: 15,
+        borderRadius: 25,
+        width: "40%",
+        alignItems: "center",
+        marginTop: 10,
+      },
 });
+
+
