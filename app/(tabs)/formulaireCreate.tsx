356b19fa54d0e7bc6d1d18494afa8e646ec1392c
Valentin Cheel <valentin.cheel.pro@gmail.com>
2025-03-03 21:23:15 +0100

    reajustement

--- app/(tabs)/formulaireCreate.tsx
+++ app/(tabs)/formulaireCreate.tsx
@@ -1,20 +1,42 @@
 import React, { useState } from "react";
 import { View, TextInput, StyleSheet, Text } from "react-native";
+import LinearGradient from 'react-native-linear-gradient';
 
 export default function MyForm() {
   const [inputText, setInputText] = useState(""); // Stocke la valeur du champ
 
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Votre Nom :</Text>
+      <Text style={styles.label}>Catégorie d'oeuvre :</Text>
       <TextInput
         style={styles.input}
-        placeholder="Entrez votre nom"
-        placeholderTextColor="#aaa" // Couleur du placeholder
-        value={inputText} // Valeur du champ
+        placeholder="Entrez le nom de la catégorie"
+        placeholderTextColor="#aaa"
+        value={inputText}
         onChangeText={setInputText} // Met à jour la valeur
       />
-      <Text style={styles.result}>Vous avez écrit : {inputText}</Text>
+
+
+
+      <Text style={styles.label}>Nom de l'oeuvre:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Entrez le nom de l'oeuvre'"
+              placeholderTextColor="#aaa"
+              value={inputText}
+              onChangeText={setInputText} // Met à jour la valeur
+            />
+
+
+      <Text style={styles.label}>Description de l'oeuvre :</Text>
+            <TextInput
+              style={styles.descriptionInput}
+              placeholder="Entrez la description de l'oeuvre"
+              placeholderTextColor="#aaa"
+              value={inputText}
+              multiline={true}
+              onChangeText={setInputText} // Met à jour la valeur
+            />
     </View>
   );
 }
@@ -24,7 +46,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f5f5f5",
+    backgroundColor: "#800080",
     padding: 20,
   },
   label: {
@@ -35,8 +57,8 @@ const styles = StyleSheet.create({
     width: "80%",
     height: 40,
     borderWidth: 2,
-    borderColor: "#FF69B4", // Rose
-    borderRadius: 10, // Bordures arrondies
+    borderColor: "#FF69B4", 
+    borderRadius: 10,
     paddingHorizontal: 10,
     backgroundColor: "white",
   },
@@ -44,5 +66,15 @@ const styles = StyleSheet.create({
     marginTop: 15,
     fontSize: 16,
     color: "black",
+  },
+  descriptionInput: {
+    width: "80%",
+    height: 100, // Augmente la hauteur
+    borderWidth: 2,
+    borderColor: "#FF69B4",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    backgroundColor: "white",
+    textAlignVertical: "top", // Le texte commence en haut
   },
 });
