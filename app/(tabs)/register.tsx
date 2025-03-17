4b124f509ff0139e06247dc97e111908e71184d3
Valentin Cheel <valentin.cheel.pro@gmail.com>
2025-03-17 16:24:07 +0100

    add page lectureBoard first version

---
+++ app/(tabs)/register.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import styles from "../styles/registerStyles";
+
+export default function MyForm() {
+  const [nomUser, setNomUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Inscription</Text>
+
+      <TextInput
+              style={myFormStyles.input}
+              placeholder="Nom d'utilisateur"
+              placeholderTextColor="#aaa"
+              value={nomUser}
+              onChangeText={setNomUser}
+            />
+
+      <TextInput
+        placeholder="E-mail"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        style={styles.input}
+        placeholderTextColor="white"
+      />
+      <TextInput
+        placeholder="Mot de passe"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.input}
+        placeholderTextColor="white"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() => console.log("Connexion")}>
+        <Text style={styles.buttonText}>S'inscrire'</Text>
+      </TouchableOpacity>
+
+      <View style={styles.linksContainer}>
+        <Text style={styles.linkText}>Pas encore de compte?</Text>
+        <Text style={styles.linkText}>Mot de passe oublié</Text>
+      </View>
+
+      <Text style={styles.socialText}>Se connecter avec :</Text>
+    </View>
+  );
+}
+
+
